8c036f6f97519e521e03b879fa5e8d97c6e56c4e
Ivan <learpcs@gmail.com>
2024-03-04 22:09:18 +0400

    Third task done

--- a.cpp
+++ a.cpp
@@ -2,9 +2,25 @@
 #include <omp.h>
 
 void first_task() {
+	std::cout << "YEAR: " <<  _OPENMP / 100 << "\nMonth: " << _OPENMP % 100 << '\n';
+}
 
+void third_task() {
+	const char *message = "Hello world";
+	#pragma omp parallel
+	{
+		#pragma omp single
+		{
+			std::cout << "Amount of threads: " << omp_get_num_threads() << std::endl;
+		}
+
+		#pragma omp barrier
+
+		std::cout << message << std::endl;
+	}
 }
 
 int main() {
-	std::cout << "YEAR: " <<  _OPENMP / 100 << "\nMonth: " << _OPENMP % 100 << '\n';
+	first_task();
+	third_task();
 }
