19acbe5f2241b7abf8bf4837a4f1c8b7b98aab6c
Ivan <learpcs@gmail.com>
2024-03-12 00:29:03 +0400

    Second task added

---
+++ b.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <omp.h>
+#include <string>
+
+void f() {
+}
+
+void first_task() {
+	std::cout << "FIRST TASK\n";
+	auto f = []() {
+		if (omp_in_parallel()) {
+			std::cout << "1" << std::endl;
+		}
+		else {
+			std::cout << "2" << std::endl;
+		}
+	};
+	#pragma omp parallel
+	{
+		#pragma omp single
+			f();
+	}
+	f();
+}
+
+void third_task() {
+	std::cout << "THIRD TASK\n";
+	#pragma omp parallel
+	{
+		#pragma omp single
+		{
+			std::cout << "Amount of threads: " << omp_get_num_threads() << std::endl;
+		}
+
+		#pragma omp barrier
+
+		std::cout << "Hello world" << std::endl;
+	}
+	std::cout << '\n';
+}
+
+void fifth_task() {
+	std::cout << "FIFTH TASK\n";
+	int x = 0;
+
+	#pragma omp parallel private(x) 
+	{
+		if (!omp_get_thread_num()) {
+			++x;
+		}
+
+		#pragma omp barrier
+
+		std::string inp = std::to_string(omp_get_thread_num()) + " " + std::to_string(x) + "\n";
+		std::cout << inp;
+	}
+	std::cout << '\n';
+}
+
+int main() {
+	std::cout << "Max threads: " << omp_get_max_threads() << std::endl << std::endl;
+	first_task();
+	third_task();
+	fifth_task();
+
+}
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ $1.cpp -fopenmp -std=c++17 -O3 -march=native -o $1
-./$1.exe
+g++ $1.cpp -fopenmp -std=c++17 -O3 -march=native -o $1.out
+./$1.out
